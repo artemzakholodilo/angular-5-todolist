1694bdff23888fada3fc27a8ec252aa41473012d
artemzakholodilo <azaholodilo@gmail.com>
2018-01-31 16:39:21 +0200

    config

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBttDiaKEo7ZqljS-UtEcBg-A_XQuT1zjQ",
+    authDomain: "todolist-97024.firebaseapp.com",
+    databaseURL: "https://todolist-97024.firebaseio.com",
+    projectId: "todolist-97024",
+    storageBucket: "todolist-97024.appspot.com",
+    messagingSenderId: "603666669899"
+  }
+};
